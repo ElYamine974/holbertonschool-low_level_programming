21867e4529d5598cac8060e0363e5736ec93708d
ElYamine974 <el-y.baco@hotmail.fr>
2022-10-10 01:39:51 +0200

    0putchar

---
+++ 9-times_table.c
@@ -0,0 +1,36 @@
+/**
+ * times_table - times_table
+ * Return: void
+ */
+void times_table(void)
+{
+    int i, j, tmp;
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            if (j != 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+                if (i * j < 10)
+                {
+                    _putchar(' ');
+                }
+            }
+            tmp = i * j;
+            if (tmp < 10)
+            {
+                _putchar(('0' + tmp));
+            }
+            else
+            {
+                _putchar(('0' + tmp / 10));
+                _putchar(('0' + tmp % 10));
+            }
+        }
+    
+    _putchar('$');
+    _putchar('\n');
+    }
+}
