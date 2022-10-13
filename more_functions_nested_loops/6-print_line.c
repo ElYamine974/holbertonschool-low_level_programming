e475a6bc0280cf9f66c0a64e9e8dfd1750326f12
ElYamine974 <113912966+ElYamine974@users.noreply.github.com>
2022-10-13 15:42:03 +0200

    Update 6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,15 @@
 #include "main"
-
 /**
 * print line -> printing line
 * @n: integer params
 */
 void print_line(int n)
-{       
-	int x;
+{
+        int x;
 
-	if (n <= 0)
-		_putchar('\n')
-	for (x = 0; x <= n; x++)
-		_putchar('_');
-	_putchar('\n');
+        if (n <= 0)
+                _putchar('\n')
+        for (x = 0; x <= n; x++)
+             _putchar('_');
+        _putchar('\n');
 }
