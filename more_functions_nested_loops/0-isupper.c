22ab2369d4c402103a9b18e9f852f6f3ee71b96b
ElYamine974 <113912966+ElYamine974@users.noreply.github.com>
2022-10-13 09:52:09 +0200

    Update 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-_isupper -> this is a function to check upper case alphas
+_isupper - this is a function to check upper case alphas
 * @c: an integer paramer
 * Return: something but success
 */
