ce1131d7db3975a388286f6170539dd2f50e6458
ElYamine974 <113912966+ElYamine974@users.noreply.github.com>
2022-10-16 23:32:34 +0200

    exo2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strchr - _strchr
+ * @s: pointer to the dest string
+ * @c: character to found
+ * Return:  pointer to the first occurrence of the
+ * character c in the string s, or NULL
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int i = 0;
+while (*(s + i) != '\0')
+{
+if (*(s + i) == c)
+{
+return (s + i);
+}
+i++;
+}
+return (NULL);
+}
