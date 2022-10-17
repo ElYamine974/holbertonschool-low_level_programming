69b3839786a0d803c6917bf604f73f22c6af10b9
ElYamine974 <113912966+ElYamine974@users.noreply.github.com>
2022-10-17 12:40:48 +0200

    Add files via upload

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion - function string the recursive
+* @: string parame
+* puts_recursion(char *s)
+*/
+
+void _puts_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{ 
+	   _putchar('\n');
+	   return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
