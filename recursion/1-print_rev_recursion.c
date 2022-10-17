5f9cb1e4b2dac62b0ab38442c00fd13089c1f4a8
ElYamine974 <113912966+ElYamine974@users.noreply.github.com>
2022-10-17 14:19:17 +0200

    Update 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,10 +2,11 @@
 /**
 * _print_rev_recursion -  string in reverse
 * @s: string s
+*Return : void 
 */
 void _print_rev_recursion (char *s)
 {
-if (*s == '0')
+if (*s == '\0')
 return;
 s++;
 _print_rev_recursion(s);
