390bf09dab31edee2e9578e4efe9392865dd04dc
ElYamine974 <113912966+ElYamine974@users.noreply.github.com>
2022-10-17 13:40:07 +0200

    Add files via upload

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _print_rev_recursion -  string in reverse
+* @s: string s
+*/
+void _print_rev_recursion (char *s)
+{
+if (*s == '0')
+return;
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
