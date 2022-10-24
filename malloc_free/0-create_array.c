b09accb919d140f9a519c48825bc290f4090021f
ElYamine974 <113912966+ElYamine974@users.noreply.github.com>
2022-10-24 16:01:55 +0200

    Add files via upload

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_array - create an array (char), initialize it with a specific char
+ * @size:
+ * @c: a variable char
+ * Return: a pointer to the array, or NULL if it falls
+ */
+char *create_array(unsigned int size, char c)
+	unsigned int i;
+	char *p;
+
+	if (size == 0) 
+{
+	return (NULL);
+}
+p= malloc(sizeof(char) * (size +1));
+if (p == NULL)
+{
+	return(NULL);
+}
+for (i = 0; i < size; i++)
+{
+	p[i] = c;
+}
+p[i] = '\0';
+
+return (p);
