54228e62d4a0dcddeb2a5641f98980969d32a043
ElYamine974 <el-y.baco@hotmail.fr>
2022-10-27 13:04:33 +0200

    strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char * _strudup(char *str)
+char * _strdup(char *str)
 {
   char * retour;
   retour = malloc(sizeof(str)+1);
-  long unsigned int i;
+  unsigned int i;
 for(i = 0; i<sizeof(str)-1;i++) 
 {
   retour[i] = str[i];
