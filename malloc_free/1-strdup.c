2e95bfc3cd3d8fc5beb0fc4edfcd167d9b270af9
ElYamine974 <el-y.baco@hotmail.fr>
2022-10-27 12:57:49 +0200

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char * _strudup(char *str)
+{
+  char * retour;
+  retour = malloc(sizeof(str)+1)
+for(int i = 0; i<sizeof(str)-1;i++) 
+{
+  retour[i] = str[i];
+}
+retour[sizeof(str)] = '\0'; //derniere case à remplir
+
+  return retour;
+
+}
+
+
+
+
+
+
