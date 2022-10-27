a8da5b3a0d657fbc04063c592304d7054e43dc5f
ElYamine974 <el-y.baco@hotmail.fr>
2022-10-27 13:15:24 +0200

    strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,17 +3,21 @@
 
 char * _strdup(char *str)
 {
-  char * retour;
- long unsigned int i =0;
-  retour = malloc(sizeof(str)+1);
-  
-  for(i = 0; i < sizeof(str) +1 ; i++) 
+char * retour;
+long unsigned int i =0;
+retour = malloc(sizeof(str)+1);
+if (retour == NULL)
+{
+	return NULL;
+}	
+for(i = 0; i < sizeof(str) +1 ; i++) 
 {
   retour[i] = str[i];
 }
 retour[sizeof(str)] = '\0';
 
-  return retour;
+free(retour);
+return retour;
 
 }
 
