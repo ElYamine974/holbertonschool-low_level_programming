912e785a144c153ce0a8020d7b7b3df92c398de4
ElYamine974 <theo@LAPTOP-FMRJR8QB.localdomain>
2022-11-08 13:11:57 +0100

    dog_t

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,10 +9,13 @@ dog_t *new_dog(char *name, float age, char *owner)
   }
   else
   {
-   dog_t *dog;
+   dog_t *dog = malloc(sizeof(dog_t));
+   if(dog != NULL)
+   {
    dog -> name = name;
    dog -> age = age;
    dog -> owner = owner;
+   }
    return dog;
   }
 
