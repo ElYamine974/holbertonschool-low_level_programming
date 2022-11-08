f79035f2a340974b569a4f4e4885a200e85b3d89
ElYamine974 <theo@LAPTOP-FMRJR8QB.localdomain>
2022-11-08 13:06:25 +0100

    dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,3 +7,10 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct 
+{
+	char* name;
+	float age;
+	char* owner;
+
+}dog_t;
